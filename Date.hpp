07ddaf63bc4f47ad71364fb51fe84b4c8172124a
SaveliyIsaev <savais2009@yandex.ru>
2025-04-25 11:42:59 +0300

    Create Date.hpp

---
+++ Date.hpp
@@ -0,0 +1,21 @@
+struct Date {
+public:
+    ull day_ = 0, month_ = 0, year_ = 0;
+    Date();
+    Date(Date& d);
+    Date(Date&& d);
+    Date(ull day, ull month, ull year);
+    bool check();
+    bool operator==(Date other);
+    bool operator!=(Date other);
+    bool operator<(Date other);
+    bool operator>(Date other);
+    bool operator<=(Date other);
+    bool operator>=(Date other);
+    Date operator++(int);
+    Date& operator++();
+    Date& operator--();
+    Date operator--(int);
+    Date operator=(Date other);
+    operator std::string() const;
+};
