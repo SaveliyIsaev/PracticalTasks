63c9a778f4279a4e4ca3cd7746aa443993308804
SaveliyIsaev <savais2009@yandex.ru>
2025-04-25 12:58:25 +0300

    Update Button.hpp

--- Button.hpp
+++ Button.hpp
@@ -3,7 +3,7 @@ struct Button {
 	sf::Font font_;
 	sf::Vector2f size_ = { 0, 0 };
 	sf::Vector2f pos_ = { 0, 0 };
-	std::string text_ = "";
+	std::u32string text_ = U"";
 	sf::Color color_ = sf::Color(50, 50, 50), text_color_ = sf::Color::Black;
 	ll text_size_ = 30;
 	bool isClicked(sf::RenderWindow& win);
