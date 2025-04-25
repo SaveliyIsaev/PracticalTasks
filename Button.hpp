ca47a1e417e17ffe94836031cdefa7f84c7274d6
SaveliyIsaev <savais2009@yandex.ru>
2025-04-25 12:53:08 +0300

    Update Button.hpp

--- Button.hpp
+++ Button.hpp
@@ -5,6 +5,7 @@ struct Button {
 	sf::Vector2f pos_ = { 0, 0 };
 	std::string text_ = "";
 	sf::Color color_ = sf::Color(50, 50, 50), text_color_ = sf::Color::Black;
+	ll text_size_ = 30;
 	bool isClicked(sf::RenderWindow& win);
 	void draw(sf::RenderWindow& window);
 	void setPosition(sf::Vector2f pos);
