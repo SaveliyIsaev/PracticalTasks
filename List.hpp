9314ca960056216a0821f4e7cebe828342e61970
SaveliyIsaev <savais2009@yandex.ru>
2025-09-25 01:24:47 +0300

    Add files via upload

--- List.hpp
+++ List.hpp
@@ -1,28 +1,144 @@
 #pragma once
 
+#ifndef LIST_HPP
+#define LIST_HPP
+
+using ll = long long;
 template<typename T>
 struct List {
-  struct Node {
-    T val;
-    Node *next = nullptr, *prev = nullptr;
-    Node() {}
-    Node(T val_) : val(val_) {}
-    ~Node() {
-      delete next;
-      next = prev = nullptr;
+    struct Node {
+        T val;
+        Node *next = nullptr, *prev = nullptr;
+        Node() {}
+        Node(T val_) : val(val_) {}
+        ~Node() {
+            delete next;
+            next = prev = nullptr;
+        }
+    } *begin = nullptr, *last = nullptr;
+    ll size = 0;
+    Node *insert(Node*, T);
+    Node *insert(Node*, Node*);
+    Node *erase(Node*);
+    List<T>& operator=(List<T>&);
+    List<T>& operator=(List<T>&&);
+    List<T>() {}
+    List<T>(ll n) {
+        for (int i = 0; i < n; ++i) insert(nullptr, new Node());
     }
-   } *begin = nullptr, *last = nullptr;
-  ll size = 0;
-  Node *insert(Node*, T);
-  Node *insert(Node*, Node*);
-  Node *erase(Node*);
-  List<T>& operator=(List<T>&);
-  List<T>& operator=(List<T>&&);
-  List<T>() {}
-  List<T>(ll n) {
-    for (int i = 0; i < n; ++i) insert(nullptr, new Node());
-  }
-  List<T>(List<T>&);
-  List<T>(List<T>&&);
-  ~List<T>();
+    List<T>(List<T>&);
+    List<T>(List<T>&&);
+    ~List<T>();
 };
+
+
+template<typename T>
+List<T>::List(List<T>& l) {
+    while (begin) erase(l.last);
+    for (List<T>::Node x = l.begin; x != nullptr; x = x->next) insert(nullptr, x->val);
+}
+
+template<typename T>
+List<T>::List(List<T>&& l) {
+    std::swap(begin, l.begin);
+    std::swap(last, l.last);
+}
+
+template<typename T>
+List<T>::~List<T>() {
+    delete begin;
+}
+
+template<typename T>
+typename List<T>::Node* List<T>::insert(List<T>::Node *next, T val) {
+    ++size;
+    if (size == 1) {
+        begin = last = new Node(val);
+        return begin;
+    }
+    if (next == begin) {
+        begin = new Node(val);
+        begin->next = next;
+        return next->prev = begin;
+    }
+    if (next == nullptr) {
+        last->next = next = new Node(val);
+        next->prev = last;
+        return last = next;
+    }
+    Node *n = new Node(val);
+    next->prev->next = n;
+    n->prev = next->prev;
+    n->next = next;
+    return next->prev = n;
+}
+
+template<typename T>
+typename List<T>::Node* List<T>::insert(Node *next, Node *n) {
+    ++size;
+    n->next = n->prev = nullptr;
+    if (size == 1) {
+        begin = last = n;
+        return begin;
+    }
+    if (next == begin) {
+        begin = n;
+        begin->next = next;
+        return next->prev = begin;
+    }
+    if (next == nullptr) {
+        last->next = next = n;
+        next->prev = last;
+        return last = next;
+    }
+    next->prev->next = n;
+    n->prev = next->prev;
+    n->next = next;
+    return next->prev = n;
+}
+
+template<typename T>
+typename List<T>::Node* List<T>::erase(Node *n) {
+    if (!n) return nullptr;
+    --size;
+    if (!size) {
+        delete n;
+        return begin = last = nullptr;
+    }
+    if (n == begin) {
+        begin = begin->next;
+        begin->prev = nullptr;
+        n->next = nullptr;
+        delete n;
+        return begin;
+    }
+    if (n == last) {
+        last = last->prev;
+        last->next = nullptr;
+        n->prev = nullptr;
+        delete n;
+        return nullptr;
+    }
+    n->next->prev = n->prev;
+    Node *next = n->prev->next = n->next;
+    n->prev = n->next = nullptr;
+    delete n;
+    return next;
+}
+
+template<typename T>
+List<T>& List<T>::operator=(List<T>& l) {
+    while (begin) erase(last);
+    for (List<T>::Node* x = l.begin; x != nullptr; x = x->next) insert(nullptr, x->val);
+    return *this;
+}
+
+template<typename T>
+List<T>& List<T>::operator=(List<T>&& l) {
+    std::swap(begin, l.begin);
+    std::swap(last, l.last);
+    return *this;
+}
+
+
+#endif // LIST_HPP
