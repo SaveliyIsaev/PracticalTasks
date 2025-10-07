a966c4fbca1f816a6aefba450fe593c4c6ef48f8
SaveliyIsaev <savais2009@yandex.ru>
2025-10-07 21:55:56 +0300

    Add files via upload

--- troll.cpp
+++ troll.cpp
@@ -1,11 +1,15 @@
 #include "troll.hpp"
 
-Troll::Troll() : time(10) {}
+Troll::Troll() : txt("img/troll.png"), time(10) {}
 
-Troll::Troll(const Troll& t) : Attacker(t), time(t.time) {}
+Troll::Troll(const Troll& t) : txt("img/troll.png"), Attacker(t), time(t.time) {}
 
-Troll::Troll(Troll&& t) : Attacker(t), time(t.time) {}
+Troll::Troll(Troll&& t) : txt("img/troll.png"), Attacker(t), time(t.time) {}
 
-Troll::Troll(ll health, ll attack, ll time) : Attacker(health, attack), time(time) {}
+Troll::Troll(ll health, ll attack, ll time) : txt("img/troll.png"), Attacker(health, attack), time(time) {}
 
 Troll::~Troll() {}
+
+void Troll::draw(sf::RenderWindow& w) {
+  w.draw(sf::Sprite(txt, sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(100, 100))));
+}
--- troll.hpp
+++ troll.hpp
@@ -9,8 +9,10 @@ class Troll : public Attacker {
   Troll(Troll&&);
   Troll(ll, ll, ll);
   ~Troll();
+  void draw(sf::RenderWindow&);
  protected:
   ll time;
+  sf::Texture txt;
 };
 
 #include "troll.cpp"
